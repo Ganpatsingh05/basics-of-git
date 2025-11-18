8562f027c2e25b4cd679d1db0f3d521aa9cd196d
GSR <151616037+Ganpatsingh05@users.noreply.github.com>
2025-11-18 13:35:29 +0530

    added salary and department

--- employee.py
+++ employee.py
@@ -4,4 +4,7 @@ ab = 4531
 print("employee ID:",ab)
 a = "Suresh"
 print("employee name: ",a)
-d = software
+d = software #department
+print("Department:",d)
+salary  = float(input("salary : "))
+print("Salary :",salary)
